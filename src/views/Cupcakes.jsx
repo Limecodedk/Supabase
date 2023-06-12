4124c9fd437831351f486fb8b7d6d1f5a6ee573f
Limecodedk <eb@limecode.dk>
2023-06-12 13:57:40 +0200

    commit "update"

--- src/views/Cupcakes.jsx
+++ src/views/Cupcakes.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { useEffect } from 'react'
+import Error from '../components/Error'
+import Loader from '../components/Loader'
+import useRequestData from '../hooks/useRequestData'
 
 const Cupcakes = () => {
+
+  const { data, isLoading, error, makeRequest } = useRequestData()
+
+  useEffect(() => {
+
+    makeRequest("https://" + process.env.REACT_APP_SUPABASEPROJECTID + ".supabase.co/rest/v1/cupcakes",
+      {
+        'apikey': process.env.REACT_APP_SUPABASEAPIKEY
+      })
+
+  }, [])
+
+
   return (
-    <div>Cupcakes</div>
+    <>
+      {error && <error />}
+
+      {isLoading && <Loader />}
+
+      {
+        data && data.map(c =>
+          <article key={c.id}>
+            <h2>{c.Name} ({"★".repeat(c.stars)})</h2>
+            <p>{c.description}</p>
+          </article>
+        )
+      }
+
+
+    </>
   )
 }
 
